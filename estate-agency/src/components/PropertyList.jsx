a24398b8ec4b8192e16c7b5beb828156140cf6f4
BeardyGeoffles <131184055+BeardyGeoffles@users.noreply.github.com>
2023-05-16 23:26:12 +0100

    container for property

--- estate-agency/src/components/PropertyList.jsx
+++ estate-agency/src/components/PropertyList.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
-import {FaShower,FaBed} from "react-icons/fa"
-import {TbGardenCart} from "react-icons/tb"
+import {TbBed, TbBath} from "react-icons/tb"
 
 export default function PropertyList() {
 
@@ -17,9 +16,15 @@ export default function PropertyList() {
         <div className="property-list">
 
                 {properties.map((item) => (
-                    <div className="property-display-short">
-                        <div>{item.address}, {item.postcode}<br></br>
-                         <FaShower /> {item.bathroom}, <FaBed /> {item.bedroom}, <TbGardenCart /> {item.garden}</div>
+                  <div className="property-display-short">
+                        <div class="container">
+                            <div class="image">
+                            <img src="https://www.saarescue.co.uk/wp-content/uploads/2017/10/Awaiting-Image.jpg" width="200px" alt="" />
+                            </div>
+                            <div class="desc">
+                            {item.address}, {item.postcode}<br></br>
+                         {item.type} <TbBath /> {item.bathroom}, <TbBed /> {item.bedroom}</div>
+                         </div>
                     </div>
                 ))}
 
