d42c1e4f505e3d1122560c6df732b1dc190da1f8
Glenn <glenn.young@dwp.gov.uk>
2023-05-16 13:06:08 +0100

    All basic lists added

---
+++ estate-agency/src/components/BookingList.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+
+export default function BookingList() {
+
+    const [bookings, setBookings] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8000/booking")
+        .then((response) => response.json())
+        .then((data) => setBookings(data))
+    }, [])
+
+    return (
+
+        <div className="booking-list">
+
+                {bookings.map((item) => (
+                    <div className="booking-display-short">
+                        <div>ID: {item.buyerId} Time: {item.time}</div>
+                    </div>
+                ))}
+
+
+        </div>
+
+    )
+}
