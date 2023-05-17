8166970cc1612f5a57a42e820336bdf1abc6ed53
Glenn <glenn.young@dwp.gov.uk>
2023-05-17 11:05:15 +0100

    Status added

--- estate-agency/src/components/PropertyList.jsx
+++ estate-agency/src/components/PropertyList.jsx
@@ -5,6 +5,17 @@ export default function PropertyList() {
 
     const [properties, setProperties] = useState([]);
 
+    function getStatus(status) {
+        switch(status) {
+            case 'FOR SALE':
+                return <div className='status-for-sale'>FOR SALE</div>
+            case 'SOLD':
+                return <div className='status-sold'>SOLD</div>
+            case 'WITHDRAWN':
+                return <div className='status-withdrawn'>WITHDRAWN</div>
+        }
+    }
+
     useEffect(() => {
         fetch("http://localhost:8080/property")
         .then((response) => response.json())
@@ -22,6 +33,7 @@ export default function PropertyList() {
                             <img src="https://www.saarescue.co.uk/wp-content/uploads/2017/10/Awaiting-Image.jpg" width="200px" alt="" />
                             </div>
                             <div class="desc">
+                                {getStatus(item.status)}<br></br>
                             {item.address}, {item.postcode}<br></br>
                          {item.type} <TbBath /> {item.bathroom}, <TbBed /> {item.bedroom}</div>
                          </div>
