0e59eb129c0f6fdf945ab323d2927a864f602dff
Geoff <geoffpinder@hotmail.com>
2023-05-17 11:52:15 +0100

    Navbar CSS tidied up

--- estate-agency/src/components/Navbar.jsx
+++ estate-agency/src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom"
-import image from "../images/logo.png"
+import logo from "../images/logo.png"
 import { BsFillHouseFill} from "react-icons/bs"
+import east from "../images/East.png"
 
 export default function Navbar() {
     return (
         
         <div className="navbar">
-            <img src={image} width="300px" alt="logo" />
-            <h1>East Properties</h1>
+            <img src={logo} width="350px" alt="logo" id="logo" />
+            <img src={east} width="300px" alt="east" id="east" />
         <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/property"><BsFillHouseFill /> Properties</Link></li>
