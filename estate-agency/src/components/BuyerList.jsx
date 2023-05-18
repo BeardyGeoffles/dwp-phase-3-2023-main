ecd6847c6ac476ea8c330c58af4bb8f8c6cb46f5
Glenn <glenn.young@dwp.gov.uk>
2023-05-18 11:16:29 +0100

    Delete added to buyer/seller

--- estate-agency/src/components/BuyerList.jsx
+++ estate-agency/src/components/BuyerList.jsx
@@ -4,12 +4,25 @@ import {AiOutlineIdcard, AiOutlineHome, AiOutlinePhone} from "react-icons/ai"
 export default function BuyerList() {
 
     const [buyers, setBuyers] = useState([]);
+    const [buyerUpdated, setBuyerUpdated] = useState(false);
 
     useEffect(() => {
         fetch("http://localhost:8080/buyer")
         .then((response) => response.json())
         .then((data) => setBuyers(data))
-    }, [])
+        setBuyerUpdated(false);
+    }, [buyerUpdated])
+
+    function buyerDelete(item){
+        if(window.confirm(`Are you sure you want to delete: ${item.firstName} ${item.surname}?`) === true){
+         fetch("http://localhost:8080/buyer/" + item.id, {
+             method: 'DELETE',
+             headers: { "Content-Type": "application/json"},
+             body: JSON.stringify(item)
+         })
+         setBuyerUpdated(true); 
+        }
+     }
 
     return (
 
@@ -20,6 +33,10 @@ export default function BuyerList() {
                         <div><AiOutlineIdcard /> {item.firstName} {item.surname}<br></br>
                         <h5><AiOutlineHome /> {item.address}<br></br>
                         <AiOutlinePhone /> {item.phone}</h5></div>
+                        <div className='property-buttons'>           
+                        <button className='edit-button'>Edit</button>
+                        <button className='delete-button' onClick={() => buyerDelete(item)}>Delete</button>
+                        </div>
                     </div>
                 ))}
 
--- estate-agency/src/components/SellerList.jsx
+++ estate-agency/src/components/SellerList.jsx
@@ -4,12 +4,25 @@ import {AiOutlineIdcard, AiOutlineHome, AiOutlinePhone} from "react-icons/ai"
 export default function SellerList() {
 
     const [sellers, setSellers] = useState([]);
+    const [sellerUpdated, setSellerUpdated] = useState(false);
 
     useEffect(() => {
         fetch("http://localhost:8080/seller")
         .then((response) => response.json())
         .then((data) => setSellers(data))
-    }, [])
+        setSellerUpdated(false);
+    }, [sellerUpdated])
+
+    function sellerDelete(item){
+        if(window.confirm(`Are you sure you want to delete: ${item.firstName} ${item.surname}?`) === true){
+         fetch("http://localhost:8080/seller/" + item.id, {
+             method: 'DELETE',
+             headers: { "Content-Type": "application/json"},
+             body: JSON.stringify(item)
+         })
+         setSellerUpdated(true); 
+        }
+     }
 
     return (
 
@@ -20,6 +33,10 @@ export default function SellerList() {
                         <div><AiOutlineIdcard /> {item.firstName} {item.surname}<br></br>
                         <h5><AiOutlineHome /> {item.address}<br></br>
                         <AiOutlinePhone /> {item.phone}</h5></div>
+                        <div className='property-buttons'>           
+                        <button className='edit-button'>Edit</button>
+                        <button className='delete-button' onClick={() => sellerDelete(item)}>Delete</button>
+                        </div>
                     </div>
                 ))}
 
