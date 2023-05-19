6d34d159cbb463fc65ab75044e1791c5ede53eaa
Glenn <glenn.young@dwp.gov.uk>
2023-05-19 12:43:50 +0100

    Booklist updated with address and buttons

--- estate-agency/src/components/booking/BookingList.jsx
+++ estate-agency/src/components/booking/BookingList.jsx
@@ -2,16 +2,48 @@ import { useEffect, useState } from 'react';
 import styles from "./booking.css"
 import {BsCalendarDate } from "react-icons/bs";
 import {BiTime } from "react-icons/bi";
+import { Link } from 'react-router-dom';
 
 export default function BookingList() {
 
     const [bookings, setBookings] = useState([]);
+    const [properties, setProperties] = useState([]);
+
+    const [bookingUpdated, setBookingUpdated] = useState([false]);
 
     useEffect(() => {
         fetch("http://localhost:8080/booking")
         .then((response) => response.json())
         .then((data) => setBookings(data))
+        setBookingUpdated(false);
+    }, [bookingUpdated])
+
+    useEffect(() => {
+        fetch("http://localhost:8080/property")
+        .then((response) => response.json())
+        .then((data) => setProperties(data))
     }, [])
+
+    function getAddress(propID)
+    {
+        let result = properties.find(prop => prop.id === propID)
+        if (result){
+            return result.address + ", " + result.postcode
+        }
+
+    }
+
+    function bookingDelete(item)
+    {
+        if(window.confirm(`Are you sure you want to delete booking on: ${new Date(item.time).toLocaleString('en-GB')}?`) === true){
+            fetch("http://localhost:8080/booking/" + item.id, {
+                method: 'DELETE',
+                headers: { "Content-Type": "application/json"},
+                body: JSON.stringify(item)
+            })
+            setBookingUpdated(true); 
+           }
+    }
 
     return (
 
@@ -21,7 +53,12 @@ export default function BookingList() {
 
                 {bookings.map((item) => (
                     <div className="booking-display-short">
-                        <div>ID: {item.id} <BsCalendarDate/> {new Date(item.time).toLocaleDateString('en-GB')} - <BiTime /> {new Date(item.time).toLocaleTimeString()}</div>
+                        <div><BsCalendarDate/> {new Date(item.time).toLocaleDateString('en-GB')} - <BiTime /> {new Date(item.time).toLocaleTimeString()}</div>
+                        <div>{getAddress(item.propertyId)}</div>
+                        <div className='property-buttons'>           
+                        <Link to={`/booking/${item.id}/edit`} state={item} className='edit-button'>Edit</Link>
+                        <button className='delete-button' onClick={() => bookingDelete(item)}>Delete</button>
+                        </div>
                     </div>
                 ))}
 
