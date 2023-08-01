13ffe71559337a1589b90243a5546d69094cb28e
Glenn <glenn.young@dwp.gov.uk>
2023-08-01 12:24:59 +0100

    Estate Agency Server added

---
+++ estate-agency-server/src/main/java/com/gago/estateagencyserver/EstateAgencyServerApplication.java
@@ -0,0 +1,13 @@
+package com.gago.estateagencyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstateAgencyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstateAgencyServerApplication.class, args);
+	}
+
+}
---
+++ estate-agency-server/src/test/java/com/gago/estateagencyserver/EstateAgencyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gago.estateagencyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstateAgencyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
